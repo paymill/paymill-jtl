c947680d765ba1db7df05a0ca0c93c7458d7f7af
Philipp Frank <philipp.alexander.frank@gmail.com>
2012-10-08 10:02:35 +0200

    Constant error (some optional constants were missing)

--- includes/modules/payment/paymill.php
+++ includes/modules/payment/paymill.php
@@ -297,6 +297,7 @@ class paymill {
 
 	function install() {
 		xtc_db_query("insert into ".TABLE_CONFIGURATION." (configuration_key, configuration_value, configuration_group_id, sort_order, set_function, date_added) values ('MODULE_PAYMENT_PAYMILL_STATUS', 'True', '6', '0', 'xtc_cfg_select_option(array(\'True\', \'False\')', now())");
+		xtc_db_query("insert into ".TABLE_CONFIGURATION." (configuration_key, configuration_value, configuration_group_id, sort_order, date_added) values ('MODULE_PAYMENT_PAYMILL_ALLOWED', '', '6', '0', now())");
 		xtc_db_query("insert into ".TABLE_CONFIGURATION." (configuration_key, configuration_value, configuration_group_id, sort_order, date_added) values ('MODULE_PAYMENT_PAYMILL_SORT_ORDER', '0', '6', '0', now())");
         xtc_db_query("insert into ".TABLE_CONFIGURATION." (configuration_key, configuration_value, configuration_group_id, sort_order, date_added) values ('MODULE_PAYMENT_PAYMILL_PUBLICKEY', '0', '6', '0', now())");
         xtc_db_query("insert into ".TABLE_CONFIGURATION." (configuration_key, configuration_value, configuration_group_id, sort_order, date_added) values ('MODULE_PAYMENT_PAYMILL_PRIVATEKEY', '0', '6', '0', now())");
@@ -308,7 +309,7 @@ class paymill {
 	}
 
 	function keys() {
-		return array('MODULE_PAYMENT_PAYMILL_STATUS',  'MODULE_PAYMENT_PAYMILL_SORT_ORDER', 'MODULE_PAYMENT_PAYMILL_PUBLICKEY', 'MODULE_PAYMENT_PAYMILL_PRIVATEKEY');
+		return array('MODULE_PAYMENT_PAYMILL_STATUS',  'MODULE_PAYMENT_PAYMILL_SORT_ORDER', 'MODULE_PAYMENT_PAYMILL_PUBLICKEY', 'MODULE_PAYMENT_PAYMILL_PRIVATEKEY', 'MODULE_PAYMENT_PAYMILL_ALLOWED');
 	}
 
     function update_status() {
--- lang/german/modules/payment/paymill.php
+++ lang/german/modules/payment/paymill.php
@@ -2,10 +2,13 @@
 define('MODULE_PAYMENT_PAYMILL_ACCEPTED_CARDS', 'Akzeptierte Kreditkarten');
 define('MODULE_PAYMENT_PAYMILL_TEXT_TITLE', 'Kreditkarte');
 define('MODULE_PAYMENT_PAYMILL_STATUS_TITLE', 'Kreditkartenmodul aktivieren');
+define('MODULE_PAYMENT_PAYMILL_STATUS_DESC', '');
 define('MODULE_PAYMENT_PAYMILL_SORT_ORDER_TITLE', 'Anzeigereihenfolge');
 define('MODULE_PAYMENT_PAYMILL_SORT_ORDER_DESC', 'Reihenfolge der Anzeige. Kleinste Ziffer wird zuerst angezeigt.');
 define('MODULE_PAYMENT_PAYMILL_PRIVATEKEY_TITLE', 'Geheimer API Key');
+define('MODULE_PAYMENT_PAYMILL_PRIVATEKEY_DESC', '');
 define('MODULE_PAYMENT_PAYMILL_PUBLICKEY_TITLE', 'Öffentlicher API Key');
+define('MODULE_PAYMENT_PAYMILL_PUBLICKEY_DESC', '');
 define('MODULE_PAYMENT_PAYMILL_TEXT_ERROR_100', 'Zahlung konnte nicht ausgeführt werden. Der Zahlungsstatus ist "open"');
 define('MODULE_PAYMENT_PAYMILL_TEXT_ERROR_200', 'Zahlung konnte nicht ausgeführt werden.');
 //
