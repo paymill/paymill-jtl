b06623db3d32a1dad784844af35d0e410e452bbb
Harenkamp <stefan.harenkamp@payintelligent.de>
2013-03-18 09:42:05 +0100

    Revert "PM-175 3D Secure anpassung: JTL - done"
    This reverts commit 66ad809173a4e4ced7e830d65ad54273ead739b4.

--- pi_paymill/version/108/frontend/payment_error.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if (array_key_exists('pi_error', $_SESSION) && array_key_exists('error', $_SESSION['pi_error'])) {
-    pq('.box_info')->text($_SESSION['pi_error']['error']);
-    pq('.box_info')->addClass('box_error');
-    pq('.box_error')->attr('style', 'border:2px solid red');
-    pq('.box_info')->removeClass('box_info');
-    unset($_SESSION['pi_error']['error']);
-}
