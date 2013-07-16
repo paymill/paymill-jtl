da0fc43f2f4ca378f4730738532fab649f36232a
kazemek <>
2013-07-16 16:20:12 +0200

    PM-422 add new version folder

---
+++ pi_paymill/version/111/frontend/payment_check.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once(dirname(__FILE__) . '/../paymentmethod/classes/helpers/Util.php');
+require_once(dirname(__FILE__) . '/../paymentmethod/classes/helpers/PaymentCheck.php');
+
+$payment = PaymentCheck::getPayment();
+if (Util::isPaymillPayment($payment->cName, $oPlugin)) {
+    $posted = array_key_exists('paymillToken', $_POST) && PaymentCheck::checkToken($_POST['paymillToken']);
+    $session = array_key_exists('paymillToken', $_SESSION['pi']) && PaymentCheck::checkToken($_SESSION['pi']['paymillToken']);
+    if ($posted || $session) {
+        PaymentCheck::setToken(!empty($_SESSION['pi']['paymillToken']) ? $_SESSION['pi']['paymillToken'] : $_POST['paymillToken']);
+    } else {
+        $_SESSION['pi_error']['error'] = 'Unerwarteter Fehler!';
+        header("Location: " . gibShopURL() . '/bestellvorgang.php?editZahlungsart=1');
+    }
+}
