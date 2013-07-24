837d932473eb2fe54b077668bd76510c67172afa
kazemek <>
2013-07-24 12:32:09 +0200

    fix double table creation

--- pi_paymill/version/111/sql/install.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `xplugin_pi_paymill_tfastcheckout` (
-  `userID` varchar(100),
-  `clientID` varchar(100),
-  `paymentID_CC` varchar(100),
-  `paymentID_ELV` varchar(100),
-  PRIMARY KEY (`userID`)
-);
