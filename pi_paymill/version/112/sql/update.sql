5b0fad152d9ff8aba9425f2c053351e86cefcc54
kazemek <>
2013-07-31 18:15:52 +0200

    PM-482 remove twice time table creation

--- pi_paymill/version/112/sql/update.sql
+++ pi_paymill/version/112/sql/update.sql
@@ -1,9 +1,1 @@
-CREATE TABLE `xplugin_pi_paymill_tfastcheckout` (
-  `userID` varchar(100),
-  `clientID` varchar(100),
-  `paymentID_CC` varchar(100),
-  `paymentID_ELV` varchar(100),
-  PRIMARY KEY (`userID`)
-);
-
 DELETE FROM xplugin_pi_paymill_tfastcheckout;
