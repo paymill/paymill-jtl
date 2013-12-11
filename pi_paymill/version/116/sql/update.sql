9201b9d15c592d6222710c0daf6261948343934f
kazemek <>
2013-12-11 09:50:48 +0100

    fix update.sql

--- pi_paymill/version/116/sql/update.sql
+++ pi_paymill/version/116/sql/update.sql
@@ -1,8 +1,8 @@
-CREATE TABLE IF NOT EXISTS `xplugin_pi_paymill_log` (
+CREATE TABLE `xplugin_pi_paymill_log` (
 `id` int(11) NOT NULL AUTO_INCREMENT,
 `identifier` text NOT NULL,
 `debug` text NOT NULL,
 `message` text NOT NULL,
 `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 PRIMARY KEY (`id`)
-) AUTO_INCREMENT=1
+) AUTO_INCREMENT=1;
