68951f4eb84c915ae7b8c7cdc4882754088eea84
edip <edip@edip-PC>
2014-03-27 11:01:49 +0100

    PMJTL-52 added JavaScript Files for CC Brand Detection and Iban Validation

--- pi_paymill/version/118/frontend/payment_selection.php
+++ pi_paymill/version/118/frontend/payment_selection.php
@@ -4,11 +4,11 @@ require_once(dirname(__FILE__) . '/../paymentmethod/classes/helpers/PaymentSelec
 $pluginPath = gibShopUrl() . "/" . PFAD_PLUGIN . $oPlugin->cVerzeichnis . "/" . PFAD_PLUGIN_VERSION . $oPlugin->nVersion . "/paymentmethod";
 $debug = ($oPlugin->oPluginEinstellungAssoc_arr['pi_paymill_debug_mode'] == 1) ? "true" : "false";
 
-foreach ($oPlugin->oPluginSprachvariableAssoc_arr as $key => $value) { 
+foreach ($oPlugin->oPluginSprachvariableAssoc_arr as $key => $value) {
     $$key = $value;
 }
 
-foreach ($oPlugin->oPluginEinstellungAssoc_arr as $key => $value) { 
+foreach ($oPlugin->oPluginEinstellungAssoc_arr as $key => $value) {
     $$key = $value;
 }
 
@@ -38,23 +38,25 @@ $js = <<<HTML
     lang['iban_invalid'] = '$Iban_Invalid';
     lang['bic_invalid'] = '$Bic_Invalid';
     // Paymill bridge js lang
-    lang['internal_server_error'] = '$PAYMILL_internal_server_error';    
-    lang['invalid_public_key'] = '$PAYMILL_invalid_public_key';    
-    lang['invalid_payment_data'] = '$PAYMILL_invalid_payment_data';    
-    lang['unknown_error'] = '$PAYMILL_unknown_error';    
-    lang['3ds_cancelled'] = '$PAYMILL_3ds_cancelled';    
-    lang['field_invalid_card_number'] = '$PAYMILL_field_invalid_card_number';    
-    lang['field_invalid_card_exp_year'] = '$PAYMILL_field_invalid_card_exp_year';    
-    lang['field_invalid_card_exp_month'] = '$PAYMILL_field_invalid_card_exp_month';    
-    lang['field_invalid_card_exp'] = '$PAYMILL_field_invalid_card_exp';    
-    lang['field_invalid_card_cvc'] = '$PAYMILL_field_invalid_card_cvc';    
-    lang['field_invalid_card_holder'] = '$PAYMILL_field_invalid_card_holder';    
-    lang['field_invalid_amount_int'] = '$PAYMILL_field_invalid_amount_int';    
-    lang['field_field_invalid_amount'] = '$PAYMILL_field_field_invalid_amount';    
-    lang['field_field_field_invalid_currency'] = '$PAYMILL_field_field_field_invalid_currency';    
+    lang['internal_server_error'] = '$PAYMILL_internal_server_error';
+    lang['invalid_public_key'] = '$PAYMILL_invalid_public_key';
+    lang['invalid_payment_data'] = '$PAYMILL_invalid_payment_data';
+    lang['unknown_error'] = '$PAYMILL_unknown_error';
+    lang['3ds_cancelled'] = '$PAYMILL_3ds_cancelled';
+    lang['field_invalid_card_number'] = '$PAYMILL_field_invalid_card_number';
+    lang['field_invalid_card_exp_year'] = '$PAYMILL_field_invalid_card_exp_year';
+    lang['field_invalid_card_exp_month'] = '$PAYMILL_field_invalid_card_exp_month';
+    lang['field_invalid_card_exp'] = '$PAYMILL_field_invalid_card_exp';
+    lang['field_invalid_card_cvc'] = '$PAYMILL_field_invalid_card_cvc';
+    lang['field_invalid_card_holder'] = '$PAYMILL_field_invalid_card_holder';
+    lang['field_invalid_amount_int'] = '$PAYMILL_field_invalid_amount_int';
+    lang['field_field_invalid_amount'] = '$PAYMILL_field_field_invalid_amount';
+    lang['field_field_field_invalid_currency'] = '$PAYMILL_field_field_field_invalid_currency';
 </script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
 <script type="text/javascript" src="https://bridge.paymill.com/"></script>
+<script type="text/javascript" src="$pluginPath/js/BrandDetection.js"></script>
+<script type="text/javascript" src="$pluginPath/js/Iban.js"></script>
 <script type="text/javascript" src="$pluginPath/js/payment.js"></script>
 <link rel="stylesheet" type="text/css" href="$pluginPath/css/paymill.css" />
 HTML;
